8a5eea4c520821edbe573104a75c52bf290d98f3
Gerardo Hernandez Bustos <gerardo.hernandezbustos@mercadolibre.com.mx>
2021-08-25 14:25:47 -0500

    ejemplo replanteamiento concurrencia
    se cambia el funcionamiento de la concurrencia desglosando mas las responsabilidades

--- main.go
+++ main.go
@@ -4,23 +4,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 
-	f, err := os.Create("mi_archivo_concurrente.txt")
-
-	defer f.Close()
+	f, err := os.Create("mi_archivo_concurrenteV2.txt")
 
 	if err != nil {
 		panic(err)
 	}
+	outCh := make(chan string)
+	doneWrite := make(chan struct{})
 
-	numGoRutines := 100
-	doneCh := make(chan struct{})
+	// write
+	go func() {
+		for s := range outCh {
+			_, err := f.WriteString(s)
+			if err != nil {
+				panic(err)
+			}
+		}
+		doneWrite <- struct{}{}
+	}()
 
+	numGoRutines := 10
+	doneCh := make(chan string)
 	final := 16777215
 
 	for i := 0; i <= final; i = i + (final / numGoRutines) + 1 {
@@ -31,28 +42,29 @@ func main() {
 		}
 
 		fmt.Printf("ejecutando %d %d\n", i, paso)
-		go calcNum(i, paso, f, doneCh)
+		go calcNum(i, paso, outCh, doneCh)
 	}
 
 	doneNum := 0
 	for doneNum < numGoRutines {
-		<-doneCh
+		rangoTerminado := <-doneCh
+		fmt.Println("termino rango ", rangoTerminado)
 		doneNum++
-		fmt.Println("termino ", doneNum)
 	}
 
+	close(outCh)
+	<-doneWrite
 	fmt.Println("listo!!")
 
 	elapsed := time.Since(start)
-	log.Printf("Concurrencia  V1 %s", elapsed)
+	log.Printf("Concurrencia V2 %s", elapsed)
 }
 
-func calcNum(start, end int, f *os.File, doneCh chan struct{}) {
+func calcNum(start, end int, resultCh chan string, doneCh chan string) {
+	var sBuilder strings.Builder
 	for i := start; i <= end; i++ {
-		_, err := f.WriteString(fmt.Sprintf("%06x\n", i))
-		if err != nil {
-			panic(err)
-		}
+		fmt.Fprint(&sBuilder, fmt.Sprintf("%06x\n", i))
 	}
-	doneCh <- struct{}{}
+	resultCh <- sBuilder.String()
+	doneCh <- fmt.Sprintf("%v al %v", start, end)
 }
